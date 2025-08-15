c7fc890c6bba2103f5ddff6cc69f8e0267a979e6
Mark-hil <80138284+Mark-hil@users.noreply.github.com>
2025-08-15 00:14:05 +0000

    Deploying to gh-pages from @ Mark-hil/Mark-Hill-Portfolio@0e6a6c9a28b2c919f2fb6bf384ed8b3b4f0ed6c0 🚀

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+/*!
+* Start Bootstrap - Personal v1.0.1 (https://Mark-hil.github.io/Mark-Hill-Portfolio)
+* Copyright 2013-2025 Mark Hill-Ampomah
+* Licensed under undefined (https://github.com/StartBootstrap/Ampomah-Mark-Hill/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
